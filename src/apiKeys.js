e42434fecba948269b4583c8651c3a9b864f6688
Sushmita Shetty <sushmitashetty8080@gmail.com>
2024-07-08 00:28:07 +0530

    commits

---
+++ src/apiKeys.js
@@ -0,0 +1,5 @@
+// Visit https://api.openweathermap.org & then signup to get our API keys for free
+module.exports = {
+  key: "0b10dc21d70ce2b6ae5c243f6137af1e",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
